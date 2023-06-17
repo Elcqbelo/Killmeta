56288011bea503353f4fe9a7058a5d0d6549da1a
Elcqbelo <69735063+Elcqbelo@users.noreply.github.com>
2023-06-16 22:37:16 -0300

    Add files via upload

---
+++ proyecto1/parte1/js/capitulos.js
@@ -0,0 +1,1 @@
+window.onload =()=>{$("#mostrar_h1").fadeIn(1800), $("#mostrar_div").fadeIn(1800), $("#mostrar_btn_caps").fadeIn(1800), $("#mostrar_btn_return").fadeIn(1800)}
---
+++ proyecto1/parte1/js/dom_test.js
@@ -0,0 +1,10 @@
+
+function sumaUno(){
+    let number = 1
+    for (n of number)
+        let carousel =  ` <li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="${}"></li> `
+        res = n + 1
+        console.log(carousel)
+    break;
+}
+sumaUno()
